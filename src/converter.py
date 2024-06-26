39263e0a8d3d294cf68372755eff83962b8b84eb
Daniel Sh. <dshamsoddini@gmail.com>
2024-06-26 12:11:42 -0700

    almost functional

--- src/converter.py
+++ src/converter.py
@@ -21,23 +21,29 @@ for row in df.iterrows():
 
     # break
     dialogue_str = ""
+    output = ""
     for a in row[1]['chat_logs']:
         #print(a)
         id = "YOU" if a['id'] == 'mturk_agent_1' else "THEM"
         if a['text'] == "Submit-Deal" or a['text'] == "Accept-Deal":
+            if a['text'] == "Submit-Deal":
+                output = f"item0={} item1={} item2={} item0={} item1{} item2={}"
             if "<selection>" not in dialogue_str:
                 dialogue_str += f"{id}: <selection>"
+        elif a['text'] == 'Walk-Away':
+            output = "<disagree> " * 6
+            print(output)
         else:
             dialogue_str += f"{id}: {a['text']} <eos> "
 
     #each item has a total quantity of 3
     input_arr = {row[1]["participant_info"]['mturk_agent_1']["value2issue"][k]:v for k,v in val2num.items()}
     partner_arr = {row[1]["participant_info"]['mturk_agent_2']["value2issue"][k]:v for k,v in val2num.items()}
-    print(row[1]["participant_info"]['mturk_agent_1']["value2issue"])
-    print(row[1]["participant_info"]['mturk_agent_2']["value2issue"])
-    print(input_arr)
-    print(partner_arr)
-    master_str += f"<input> 3 {input_arr["Firewood"]} 3 {input_arr["Food"]} 3 {input_arr["Water"]} </input> <dialogue> {dialogue_str} </dialogue> <output> 1 </output> <partner_input> 3 {partner_arr["Firewood"]} 3 {partner_arr["Food"]} 3 {partner_arr["Water"]} </partner_input>\n"
+    # print(row[1]["participant_info"]['mturk_agent_1']["value2issue"])
+    # print(row[1]["participant_info"]['mturk_agent_2']["value2issue"])
+    # print(input_arr)
+    # print(partner_arr)
+    master_str += f"<input> 3 {input_arr["Firewood"]} 3 {input_arr["Food"]} 3 {input_arr["Water"]} </input> <dialogue> {dialogue_str} </dialogue> <output> {output}</output> <partner_input> 3 {partner_arr["Firewood"]} 3 {partner_arr["Food"]} 3 {partner_arr["Water"]} </partner_input>\n"
     print(row[0])
 
 with open('data/casino/casino_convo.txt', 'w', encoding="utf-8") as log_file:
